4af9607c4005f7a6c2e466f302e6791b247e13a7
liana-github <hailina371@gmail.com>
2024-01-05 14:14:41 -0800

    wct2 final

---
+++ WCT_V2_Final/app/config.py
@@ -0,0 +1,16 @@
+import os
+
+DEBUG = True
+# TESTING = True
+MYSQL_HOST = '127.0.0.1'
+MYSQL_USER = 'root'
+MYSQL_PORT = 33062
+MYSQL_PASSWORD = '1234'
+MYSQL_DB = 'tracker'
+MYSQL_CURSORCLASS = 'DictCursor'
+SECRET_KEY = 'your_secret_key'
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 1025
+MAIL_USE_TLS = True
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
